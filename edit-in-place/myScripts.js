02cbfc79846f73f3ee84a2438d35ff764ba15cbe
justin mills <jbmills@justins-MacBook-Pro.local>
2016-10-04 11:49:32 -0600

    progress on Edit In Place

--- edit-in-place/myScripts.js
+++ edit-in-place/myScripts.js
@@ -1,5 +1,20 @@
-var app = angular.module("myApp", []);
-app.controller("MainController", function($scope) {
+angular.module("myApp", [])
+    .controller("MainCtrl", mainController);
 
+function mainController() {
+    var main = this;
 
-});
+    main.name = false;
+    main.namePeople = "test";
+
+    main.runThis = function() {
+        console.log("This is working");
+        main.name = !main.name;
+    }
+    main.enterKey = function(keyEvent) {
+        if (keyEvent.which === 13) {
+            return main.runThis();
+            // console.log("Enter key is working");
+        };
+    }
+}
