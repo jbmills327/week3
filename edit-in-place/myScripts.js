83ac9f6b313f093da235d042c359f1c2cc044398
justin mills <jbmills@justins-MacBook-Pro.local>
2016-10-04 12:21:42 -0600

    Finished Edit in Place

--- edit-in-place/myScripts.js
+++ edit-in-place/myScripts.js
@@ -5,10 +5,12 @@ function mainController() {
     var main = this;
 
     main.name = false;
-    main.namePeople = "test";
+    main.namePeople = "Enter you name";
+    main.nameAge = "Age";
+    main.favoriteCareBear = "Favorite Care Bear";
 
     main.runThis = function() {
-        console.log("This is working");
+        // console.log("This is working");
         main.name = !main.name;
     }
     main.enterKey = function(keyEvent) {
