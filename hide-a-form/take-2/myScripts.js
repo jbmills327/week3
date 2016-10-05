367465c170a07dd0ccf17bdd728cce19ba15f982
justin mills <jbmills@justins-MacBook-Pro.local>
2016-10-05 14:36:22 -0600

    Finished take 2 of hide a form

--- hide-a-form/take-2/myScripts.js
+++ hide-a-form/take-2/myScripts.js
@@ -1,34 +1,32 @@
+//with 2 arguments we are creating a module and with 1 argument we are calling a module.
 angular.module("myApp", [])
     .controller("MainCtrl", mainController);
 
+
 function mainController() {
-
     var main = this;
-    // main.text = 'hello';
-    // main.list = [];
-    // main.person = {
-    //     "name": "",
-    //     "bio": "",
-    //     "book": "",
-    //     "library": ""
-    // };
-    main.name = "";
-    main.bio = "";
-    main.book = "";
-    main.library = "";
+    main.hideTheThings = false;
+    main.profileInfo = [];
+    main.newPerson = {
+        "name": "",
+        "bio": "",
+        "book": "",
+        "library": ""
+    }
+    main.addInfo = function() {
+        main.profileInfo.push(main.newPerson);
 
-    main.submit = function() {
-            console.log("This is working");
-            main.name = "";
-            main.bio = "";
-            main.book = "";
-            main.library = "";
-        }
-        // main.submit = function() {
-        //     if (main.text) {
-        //         main.list.push(main.text);
-        //         main.text = "";
-        //
-        //     };
-        // };
+        // manually trigger modal;
+        $("#myModal").modal("hide");
+        main.hideTheThings = !hideTheThings;
+
+        // give person a new object
+        // main.newPerson = {};
+
+
+
+
+
+    }
+
 }
