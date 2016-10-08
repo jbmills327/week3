f7d7e0237b429b888770ce14fc20a455c8461a05
justin mills <jbmills@justins-MacBook-Pro.local>
2016-10-08 15:07:32 -0600

    Finsihed loop practice 4

---
+++ loopPractice4/myScripts.js
@@ -0,0 +1,73 @@
+var months = [{
+    name: 'January',
+    days: 31
+}, {
+    name: 'February',
+    days: 28
+}, {
+    name: 'March',
+    days: 31
+}, {
+    name: 'April',
+    days: 30
+}, {
+    name: 'May',
+    days: 31
+}, {
+    name: 'June',
+    days: 30
+}, {
+    name: 'July',
+    days: 31
+}, {
+    name: 'August',
+    days: 31
+}, {
+    name: 'September',
+    days: 30
+}, {
+    name: 'October',
+    days: 31
+}, {
+    name: 'November',
+    days: 30
+}, {
+    name: 'December',
+    days: 31
+}];
+
+
+// This function prints the number of days for each month
+
+
+
+// function allTheDays(months) {
+//     for (var i = 0; i < months.length; i++) {
+//         console.log(months[i].name)
+//         for (var j = 0; j <= months[i].days; j++) {
+//
+//             console.log(j);
+//         }
+//     }
+// }
+//
+// allTheDays(months);
+
+
+
+// This functin returns the hours minutes and seconds in the form of a 24 hour clock
+
+
+// function allTheSeconds() {
+//     for (var i = 0; i < 24; i++) {
+//         for (var j = 0; j < 60; j++) {
+//             for (var k = 0; k < 60; k++) {
+//                 console.log(i + ":" + j + ":" + k);
+//             }
+//         }
+//     }
+//
+//
+// }
+//
+// allTheSeconds();
